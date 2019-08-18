748de89ab50859ad2e3cdc61b87716eec1c6f4b1
Ю.Лыгин <yuriylygin@gmail.com>
2019-08-18 13:05:49 +0300

    first task solution

--- req/friends.py
+++ req/friends.py
@@ -1,7 +1,29 @@
 def calc_age(uid):
-    pass
+    access_token = '994f2b77994f2b77994f2b77549923276b9994f994f2b77c40a58f91fc51ab0518224f8'
+
+    users_params = dict(user_ids=uid,
+                        fields='bdate',
+                        access_token=access_token,
+                        v='5.71')
+
+    vk_user = requests.get('https://api.vk.com/method/users.get', params=users_params)
+
+    friends_params = dict(user_id=vk_user.json()['response'][0]['id'],
+                          fields='bdate',
+                          access_token=access_token,
+                          v='5.71')
+
+    vk_user_freinds = requests.get('https://api.vk.com/method/friends.get', params=friends_params)
+
+    print(vk_user.json(), '\n')
+    # print(vk_user.json()['response'], '\n')
+    print(vk_user_freinds.json(), '\n')
+    items = vk_user_freinds['response']['items']
+    print(items)
 
 
 if __name__ == '__main__':
-    res = calc_age('reigning')
+    import requests
+    # res = calc_age('reigning')
+    res = calc_age('id11613')
     print(res)
