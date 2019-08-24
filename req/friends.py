3b58faeba8fe27b15553cee9cd29fc326fce6b8a
Yuriy Lygin <yuriylygin@gmail.com>
2019-08-24 12:58:31 +0300

    Практика по requests. The task has been resolved with usage of numpy module

--- req/friends.py
+++ req/friends.py
@@ -1,4 +1,6 @@
 def calc_age(uid):
+    import requests
+
     access_token = '994f2b77994f2b77994f2b77549923276b9994f994f2b77c40a58f91fc51ab0518224f8'
 
     users_params = dict(user_ids=uid,
@@ -15,15 +17,27 @@ def calc_age(uid):
 
     vk_user_freinds = requests.get('https://api.vk.com/method/friends.get', params=friends_params)
 
-    print(vk_user.json(), '\n')
-    # print(vk_user.json()['response'], '\n')
-    print(vk_user_freinds.json(), '\n')
-    items = vk_user_freinds['response']['items']
-    print(items)
+    import re
+    from datetime import datetime
+    import numpy as np
+
+    now = datetime.now()
+    friends_birth_years = []
+    for friend in vk_user_freinds.json()['response']['items']:
+        if 'bdate' in friend and re.match(r"[0-9]+.[0-9]+.[0-9]+", friend['bdate']):
+            byear = int(friend['bdate'].split(sep='.')[2])
+            friends_birth_years.append(now.year - byear)
+
+    years, count = np.unique(np.array(friends_birth_years),
+                             return_counts=True)
+
+    years_count = list(zip(years, count))
+    years_count.sort(key=lambda x: x[1], reverse=True)
+
+    return years_count
 
 
 if __name__ == '__main__':
-    import requests
-    # res = calc_age('reigning')
-    res = calc_age('id11613')
+    res = calc_age('reigning')
+    # res = calc_age('id11613')
     print(res)
