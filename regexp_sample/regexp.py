374a9aee52049b6534d0e49960c11a6505fbb142
Yuriy Lygin <yuriylygin@gmail.com>
2019-09-10 23:00:19 +0300

    First week. Regular expressions task solved.

--- regexp_sample/regexp.py
+++ regexp_sample/regexp.py
@@ -1,7 +1,12 @@
 def calculate(data, findall):
-    matches = findall(r"")  # Если придумать хорошую регулярку, будет просто
-    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
-        # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
+    matches = findall(r"([abc])(\+=|-=|=)([abc]?)([\+-]?[0-9]+)?")  # Если придумать хорошую регулярку, будет просто
+    print(matches)
+    for v1, s, v2, n in matches:
+        if s == '=':
+            data[v1] = data.get(v2, 0) + int(n or 0)
+        elif s == '+=':
+            data[v1] += data.get(v2, 0) + int(n or 0)
+        elif s == '-=':
+            data[v1] -= data.get(v2, 0) + int(n or 0)
 
     return data
--- regexp_sample/test.py
+++ regexp_sample/test.py
@@ -2,7 +2,7 @@ import re
 from regexp import calculate
 
 
-def findall(regexp):
+def findall_0(regexp):
     text = """
     a=1
     a=+1
@@ -29,9 +29,26 @@ def findall(regexp):
     return re.findall(regexp, text)
 
 
-result = calculate({'a': 1, 'b': 2, 'c': 3}, findall)
-correct = {"a": -98, "b": 196, "c": -686}
-if result == correct:
-    print ("Correct")
-else:
-    print ("Incorrect: %s != %s" % (result, correct))
+def findall_5(regexp):
+    text = 'y+=y+100a=+1x-=y+101a=b+100y+=y-100y+=yx-=101x=1a=bx=y'
+
+    return re.findall(regexp, text)
+
+
+if __name__ == '__main__':
+    print('test 0')
+    result = calculate({'a': 1, 'b': 2, 'c': 3}, findall_0)
+    correct = {"a": -98, "b": 196, "c": -686}
+    if result == correct:
+        print("Correct\n")
+    else:
+        print("Incorrect: %s != %s\n" % (result, correct))
+
+    print('test 5')
+    result = calculate({'a': 10, 'b': 20, 'c': 30}, findall_5)
+    correct = {'a': 20, 'b': 20, 'c': 30}
+    if result == correct:
+        print("Correct\n")
+    else:
+        print("Incorrect: %s != %s\n" % (result, correct))
+
