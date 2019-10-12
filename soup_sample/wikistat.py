2df4d60ad564c32e08ebdf720e05c96eca0bcbc5
Yuriy Lygin <yuriylygin@gmail.com>
2019-10-12 17:53:33 +0300

    [soup_sample] Second week. Solution from coursera masters

--- soup_sample/wikistat.py
+++ soup_sample/wikistat.py
@@ -3,46 +3,59 @@ import re
 import os
 
 
-# Вспомогательная функция, её наличие не обязательно и не будет проверяться
-def build_tree(start, end, path):
-    link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
-    files = dict.fromkeys(os.listdir(path))  # Словарь вида {"filename1": None, "filename2": None, ...}
-    # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
-    return files
-
-
-# Вспомогательная функция, её наличие не обязательно и не будет проверяться
 def build_bridge(start, end, path):
+    def build_tree(start, end, path):
+        link_re = re.compile(r"(?<=/wiki/)[\w()]+")
+        files = dict.fromkeys(os.listdir(path), False)
+        current_links = [start]
+        while current_links:
+            new_links = []
+            for name in current_links:
+                with open("{}{}".format(path, name)) as data:
+                    links = re.findall(link_re, data.read())
+                for link in links:
+                    if files.get(link) is False:
+                        files[link] = name
+                        if link == end:
+                            return files
+                        new_links.append(link)
+            current_links = new_links
+
     files = build_tree(start, end, path)
-    bridge = []
-    # TODO Добавить нужные страницы в bridge
+    current_link, bridge = end, [end]
+    while current_link != start:
+        current_link = files[current_link]
+        bridge.append(current_link)
     return bridge
 
 
 def parse(start, end, path):
-    """
-    Если не получается найти список страниц bridge, через ссылки на которых можно добраться от start до end, то,
-    по крайней мере, известны сами start и end, и можно распарсить хотя бы их: bridge = [end, start]. Оценка за тест,
-    в этом случае, будет сильно снижена, но на минимальный проходной балл наберется, и тест будет пройден.
-    Чтобы получить максимальный балл, придется искать все страницы. Удачи!
-    """
+    bridge = build_bridge(start, end, path)
 
-    bridge = build_bridge(start, end, path)  # Искать список страниц можно как угодно, даже так: bridge = [end, start]
-
-    # Когда есть список страниц, из них нужно вытащить данные и вернуть их
     out = {}
     for file in bridge:
         with open("{}{}".format(path, file)) as data:
             soup = BeautifulSoup(data, "lxml")
+            body = soup.find(id="bodyContent")
 
-        body = soup.find(id="bodyContent")
+            imgs = len(body.find_all('img', width=lambda x: int(x or 0) > 199))
+            headers = sum(
+                1 for tag in body.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']) if tag.get_text()[0] in "ETC"
+            )
+            lists = sum(1 for tag in body.find_all(['ol', 'ul']) if not tag.find_parent(['ol', 'ul']))
 
-        # TODO посчитать реальные значения
-        imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
-        headers = 10  # Количество заголовков, первая буква текста внутри которого: E, T или C
-        linkslen = 15  # Длина максимальной последовательности ссылок, между которыми нет других тегов
-        lists = 20  # Количество списков, не вложенных в другие списки
+            tag = body.find_next("a")
+            linkslen = -1
+            while (tag):
+                curlen = 1
+                for tag in tag.find_next_siblings():
+                    if tag.name != 'a':
+                        break
+                    curlen += 1
+                if curlen > linkslen:
+                    linkslen = curlen
+                tag = tag.find_next("a")
 
-        out[file] = [imgs, headers, linkslen, lists]
+            out[file] = [imgs, headers, linkslen, lists]
 
     return out
