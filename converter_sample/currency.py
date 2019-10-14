ba98fb2c9e99d5e671c913796c1b6aa4d736d61a
Yuriy Lygin <yuriylygin@gmail.com>
2019-10-14 20:20:46 +0300

    [converter_sample] Second week. Task 'Currency converter' solution from coursera masters

--- converter_sample/currency.py
+++ converter_sample/currency.py
@@ -3,7 +3,15 @@ from decimal import Decimal
 
 
 def convert(amount, cur_from, cur_to, date, requests):
-    response = requests.get()  # Использовать переданный requests
-    # ...
-    result = Decimal('3754.8057')
-    return result  # не забыть про округление до 4х знаков после запятой
+    result = requests.get("https://www.cbr.ru/scripts/XML_daily.asp", {"date_req": date})
+    soup = BeautifulSoup(result.content, 'xml')
+    rates = {i.CharCode.string: (
+        Decimal(i.Value.string.replace(',', '.')),
+        int(i.Nominal.string)
+    ) for i in soup('Valute')
+    }
+    rates['RUR'] = (Decimal(1), 1)
+
+    result = amount * rates[cur_from][0] * rates[cur_to][1] / rates[cur_from][1] / \
+             rates[cur_to][0]
+    return result.quantize(Decimal('.0001'))
