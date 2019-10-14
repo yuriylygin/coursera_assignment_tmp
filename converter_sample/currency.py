f84934aab5c885ff3fc804c405f3c1a5dbeb1387
Yuriy Lygin <yuriylygin@gmail.com>
2019-10-14 20:18:33 +0300

    [converter_sample] Second week. Task 'Currency converter' solved

--- converter_sample/currency.py
+++ converter_sample/currency.py
@@ -2,8 +2,41 @@ from bs4 import BeautifulSoup
 from decimal import Decimal
 
 
-def convert(amount, cur_from, cur_to, date, requests):
-    response = requests.get()  # Использовать переданный requests
-    # ...
-    result = Decimal('3754.8057')
-    return result  # не забыть про округление до 4х знаков после запятой
+def convert(amount, cur_from_, cur_to_, date, request):
+    class Currency:
+        def __init__(self, XML_daily, charcode='RUR', ID=None, nominal=1, value=1):
+            self.charcode = charcode
+            self.ID = ID
+            self.nominal = nominal
+            self.value = value
+            self.from_cbr_XML(XML_daily)
+
+        def from_cbr_XML(self, XML_daily):
+            if 'RUR' != self.charcode:
+                for currency in XML_daily.find_all('valute'):
+                    if self.charcode in currency.charcode.text:
+                        self.ID = currency.attrs['id']
+                        self.nominal = Decimal(currency.nominal.text)
+                        self.value = Decimal(currency.value.text.replace(',', '.'))
+
+        def __repr__(self):
+            return 'charcode={}, ID={}, nominal={}, value={}'.format(self.charcode,
+                                                                     self.ID,
+                                                                     self.nominal,
+                                                                     self.value)
+
+        def to_currency(self, amount):
+            return Decimal(amount) * self.nominal / self.value
+
+        def from_currency(self, amount):
+            return Decimal(amount) * self.value / self.nominal
+
+    host = 'https://www.cbr.ru/scripts/XML_daily.asp'
+    params = dict(date_req=date)
+    XML_daily = BeautifulSoup(request.get(host, params=params).text, 'lxml').valcurs
+
+    result = Currency(XML_daily, charcode=cur_to_).to_currency(
+        Currency(XML_daily, charcode=cur_from_).from_currency(amount)
+    )
+
+    return result.quantize(Decimal('.0001'))  # не забыть про округление до 4х знаков после запятой
--- converter_sample/test.py
+++ converter_sample/test.py
@@ -1,11 +1,37 @@
 import requests
 from decimal import Decimal
+import requests
+from requests import request
 from currency import convert
 
 
-correct = Decimal('3754.8057')
-result = convert(Decimal("1000.1000"), 'RUR', 'JPY', "17/02/2005", requests)
-if result == correct:
-    print("Correct")
-else:
-    print("Incorrect: %s != %s" % (result, correct))
+if __name__ == '__main__':
+    correct = Decimal('3754.8057')
+    result = convert(Decimal("1000.1000"), 'RUR', 'JPY', "17/02/2005", requests)
+    if result == correct:
+        print("Correct")
+    else:
+        print("Incorrect: %s != %s" % (result, correct))
+
+
+    class Request:
+        @staticmethod
+        def get(host, params):
+            return request('GET', f'{host}?date_req={params["date_req"]}')
+
+
+    test_cases = (
+        (10 ** 3, 'EUR', 'USD', "26/02/2017", '1051.8006'),
+        (10 ** 3, 'RUR', 'USD', "26/01/2016", '12.8540'),
+        (10 ** 3, 'USD', 'RUR', "02/02/2017", '60309.9000'),
+        (10 ** 3, 'USD', 'EUR', "10/03/2018", '805.3478'),
+        (10 ** 4, 'RUR', 'USD', "07/04/2018", '172.9111'),
+        (10 ** 3, 'KZT', 'XDR', "02/12/2016", '2.1745'),
+        (10 ** 6, 'CHF', 'USD', "15/10/2017", '1025220.5847'),
+        (10 ** 6, 'RUR', 'JPY', "26/11/2018", '1718611.7055'),
+    )
+    for amount, cur_from, cur_to, date, expected in test_cases:
+        assert str(convert(amount, cur_from, cur_to, date, Request)) == expected, \
+            f'Fail. Test cases - {amount}, {cur_from}, {cur_to}, {date}, {expected}'
+
+    print('All tests - Ok!')
