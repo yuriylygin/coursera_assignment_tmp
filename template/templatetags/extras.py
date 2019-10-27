bdd299bf407b067933f4a693bbf23377ca08d8d9
Yuriy Lygin <yuriylygin@gmail.com>
2019-10-27 16:39:31 +0300

    [template] tags, html templates, html inheritance task has been solved. Week 4 task 3

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,18 @@
 from django import template
+
+register = template.Library()
+
+
+@register.filter(name='inc')
+def increment(value, arg):
+    return int(value) + int(arg)
+
+
+@register.simple_tag
+def division(a, b, **kwargs):
+    a, b = int(a), int(b)
+    if 'to_int' in kwargs and kwargs['to_int']:
+        return int(a / b)
+    else:
+        return a / b
+
--- template/views.py
+++ template/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django import template
+from django.views.decorators.csrf import csrf_exempt
+from pprint import pprint
 
 
-# Create your views here.
+register = template.Library()
 
+
+@register.filter
+def lower(value):
+    return value.lower()
+
+
+@csrf_exempt
 def echo(request):
-    pass
+    GET_params = [item for item in request.GET.items()]
+    POST_params = [item for item in request.POST.items()]
+    return render(request, 'echo.html', context={
+        'http_method': request.method,
+        'GET_params': GET_params,
+        'POST_params': POST_params,
+        'HTTP_X_PRINT_STATEMENT': request.META.get('HTTP_X_PRINT_STATEMENT')
+    })
 
 
 def filters(request):
