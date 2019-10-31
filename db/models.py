03ce9b1703c900ee8caa37d063da5c0052fca0b1
Yuriy Lygin <yuriylygin@gmail.com>
2019-10-31 11:03:31 +0300

    [db] Django ORM task solved. Week 4

--- db/models.py
+++ db/models.py
@@ -9,6 +9,9 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return str(self.id) + ',' + self.first_name + ',' + self.last_name
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
@@ -16,6 +19,9 @@ class Blog(models.Model):
     created = models.DateTimeField(default=timezone.now)
 
     subscribers = models.ManyToManyField(User, related_name='subscriptions')
+
+    def __str__(self):
+        return str(self.id) + ',' + self.title + ',' + str(self.author.id)
 
 
 class Topic(models.Model):
@@ -25,3 +31,28 @@ class Topic(models.Model):
     created = models.DateTimeField(default=timezone.now)
 
     likes = models.ManyToManyField(User, related_name='likes')
+
+    def __str__(self):
+        return str(self.id) + ',' + self.title + ',' + str(self.blog.id) + ',' \
+               + str(self.author.id) + ',' + str(self.created) + ',' + str(self.likes)
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Mata:
+        ordering = ('headline',)
+
+    def __str__(self):
+        return self.headline
--- db/query.py
+++ db/query.py
@@ -6,61 +6,94 @@ from pytz import UTC
 from db.models import User, Blog, Topic
 
 
+def clear():
+    User.objects.all().delete()
+    Blog.objects.all().delete()
+    Topic.objects.all().delete()
+
+
 def create():
-    pass
+    u1 = User(first_name='u1', last_name='u1')
+    u2 = User(first_name='u2', last_name='u2')
+    u3 = User(first_name='u3', last_name='u3')
+
+    u1.save()
+    u2.save()
+    u3.save()
+
+    blog1 = Blog(title='blog1', author=u1)
+    blog2 = Blog(title='blog2', author=u1)
+    blog1.save()
+    blog2.save()
+    blog1.subscribers.add(u1, u2)
+    blog2.subscribers.add(u2)
+
+    topic1 = Topic(title='topic1', blog=blog1, author=u1)
+    topic2 = Topic(title='topic2_content', blog=blog1, author=u3, created='2017-01-01')
+    topic1.save()
+    topic2.save()
+    topic1.likes.add(u1, u2, u3)
 
 
 def edit_all():
-    pass
+    for user in User.objects.all():
+        user.first_name = 'uu1'
+        user.save()
 
 
 def edit_u1_u2():
-    pass
+    for user in User.objects.filter(Q(first_name='u1') | Q(first_name='u2')):
+        user.first_name = 'uu1'
+        user.save()
 
 
 def delete_u1():
-    pass
+    for user in User.objects.filter(first_name='u1'):
+        user.delete()
 
 
 def unsubscribe_u2_from_blogs():
-    pass
+    for user in User.objects.filter(first_name='u2'):
+        for blog in Blog.objects.all():
+            blog.subscribers.remove(user)
 
 
 def get_topic_created_grated():
-    pass
+    return Topic.objects.filter(created__gt='2018-01-01')
 
 
 def get_topic_title_ended():
-    pass
+    return Topic.objects.filter(title__endswith='content')
 
 
 def get_user_with_limit():
-    pass
+    return User.objects.all().order_by('-id')[:2]
 
 
 def get_topic_count():
-    pass
+    return Blog.objects.annotate(topic_count=Count('topic')).order_by('topic_count')
 
 
 def get_avg_topic_count():
-    pass
+    return Blog.objects.annotate(topic_count=Count('topic')).aggregate(avg=Avg('topic_count'))
 
 
 def get_blog_that_have_more_than_one_topic():
-    pass
+    return Blog.objects.annotate(topic_count=Count('topic')).filter(topic_count__gt=1)
 
 
 def get_topic_by_u1():
-    pass
+    return Topic.objects.filter(author__first_name='u1')
 
 
 def get_user_that_dont_have_blog():
-    pass
+    return User.objects.filter(blog__author__isnull=True)
 
 
 def get_topic_that_like_all_users():
-    pass
+    amount_of_users = User.objects.all().aggregate(count=Count('pk'))['count']
+    return Topic.objects.annotate(count_likes_user=Count('likes')).filter(count_likes_user=amount_of_users)
 
 
 def get_topic_that_dont_have_like():
-    pass
+    return Topic.objects.annotate(count_likes_user=Count('likes')).filter(count_likes_user=0)
