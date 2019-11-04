3f61f488cf3814e36cd23d046b59269bb7ca51bd
Yuriy Lygin <yuriylygin@gmail.com>
2019-11-04 16:43:05 +0300

    [coursera_forms] week 5. Posting data from browser

---
+++ courera_forms/formdummy/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class FormDummyView(View):
+
+    def get(self, request):
+        return render(request, 'form.html', {})
+
+
