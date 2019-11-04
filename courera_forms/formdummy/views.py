3aaafebf6332492d4eafc6a7d09201d6654db3ed
Yuriy Lygin <yuriylygin@gmail.com>
2019-11-04 18:16:12 +0300

    [formdummy] week 5. Creating get and post methods in django.views.View class to process GET and POST methods from browser

--- courera_forms/formdummy/views.py
+++ courera_forms/formdummy/views.py
@@ -7,4 +7,14 @@ class FormDummyView(View):
     def get(self, request):
         return render(request, 'form.html', {})
 
+    def post(self, request):
+        text = request.POST.get('text')
+        grade = request.POST.get('grade')
+        content = request.FILES.get('image').read()
+
+        return render(request, 'form.html', context=dict(
+            text=text,
+            grade=grade,
+            content=content,
+        ))
 
