f809894f76a4f508afc8c9d8366c6129e8afb5fb
Yuriy Lygin <yuriylygin@gmail.com>
2019-11-04 22:48:27 +0300

    Revert "[req] week 5. Using JsonSchema validation in Django"
    This reverts commit d57256d9

--- courera_forms/formdummy/urls.py
+++ courera_forms/formdummy/urls.py
@@ -17,5 +17,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SchemaView.as_view())
+    path('', views.FormDummyView.as_view())
 ]
--- courera_forms/formdummy/views.py
+++ courera_forms/formdummy/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
 from django import views
-from django.http import JsonResponse
-
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-
-from jsonschema import validate
-from jsonschema.exceptions import ValidationError
-import json
-
-from .schemas import REVIEW_SCHEMA
 from .forms import DummyForm
 
 
@@ -29,21 +19,3 @@ class FormDummyView(views.View):
             return render(request, 'form.html', context=context)
         else:
             return render(request, 'error.html', context={'error': form.errors})
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class SchemaView(views.View):
-
-    def post(self, request):
-        try:
-            # from pdb import set_trace; set_trace()
-            document = json.loads(request.body)
-            validate(document, REVIEW_SCHEMA)
-            return JsonResponse(document, status=201)
-
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON'}, status=400)
-
-        except ValidationError as exc:
-            return JsonResponse({'error': exc.message}, status=400)
-
